0461673a5a6298cd02480f2125aeb8f12e33bccd
Justin Bachorik <justinbach@gmail.com>
2018-01-21 08:09:55 -0500

    Implement top-level h:head, h:ref, and h:link conversion

--- test/unit/hal-reverse.js
+++ test/unit/hal-reverse.js
@@ -3,29 +3,59 @@
  */
 
 const test = require('blue-tape');
-const log = require('metalogger');
 const kokua = require('../../lib/kokua');
-const halTranslator = require('../../lib/plugins/hal-reverse');
 const _ = require('lodash');
 const { loadFixture } = require('../helpers/fixture-helper');
 
-let halDoc;
-let expectedHyperDoc;
-
-const setup = async () => {
-  const rawExpectedHyperDoc = await loadFixture('hal-hyper.json');  
-  expectedHyperDoc = JSON.parse(rawExpectedHyperDoc);
+const loadFixtures = async () => {
+  const rawHyperDoc = await loadFixture('hal-hyper.json');  
+  const hyperDoc = JSON.parse(rawHyperDoc);
 
   const rawHalDoc = await loadFixture('hal.json');
-  halDoc = JSON.parse(rawHalDoc);
+  const halDoc = JSON.parse(rawHalDoc);
+
+  return { hyperDoc, halDoc };
 };
 
-test('HAL to Hyper: Full test', async (t) => {
-  await setup();
+test.skip('HAL to Hyper: Full test', async (t) => {
+  const { hyperDoc: expectedHyperDoc, halDoc } = await loadFixtures();
   
   const actualHyperDoc = kokua.parse(halDoc, kokua.mt('hal'));
 
   t.same(actualHyperDoc, expectedHyperDoc, 
     'The translated and the test messages are equivalent'
+  );
+});
+
+test('HAL to Hyper: h:head', async (t) => {
+  const { hyperDoc: expectedHyperDoc, halDoc } = await loadFixtures();
+  const headPath = '["h:head"]';
+
+  const actualHyperDoc = kokua.parse(halDoc, kokua.mt('hal'));
+
+  t.same(_.get(actualHyperDoc, headPath), _.get(expectedHyperDoc, headPath),
+    'The translated and test messages have equivalent h:head properties'
+  );
+});
+
+test('HAL to Hyper: h:ref', async (t) => {
+  const { hyperDoc: expectedHyperDoc, halDoc } = await loadFixtures();
+  const refPath = '["h:ref"]';
+
+  const actualHyperDoc = kokua.parse(halDoc, kokua.mt('hal'));
+
+  t.same(_.get(actualHyperDoc, refPath), _.get(expectedHyperDoc, refPath),
+    'The translated and test messages have equivalent h:ref properties'
+  );
+});
+
+test('HAL to Hyper: h:link', async (t) => {
+  const { hyperDoc: expectedHyperDoc, halDoc } = await loadFixtures();
+  const refPath = '["h:link"]';
+
+  const actualHyperDoc = kokua.parse(halDoc, kokua.mt('hal'));
+
+  t.same(_.get(actualHyperDoc, refPath).sort(), _.get(expectedHyperDoc, refPath).sort(),
+    'The translated and test messages have equivalent h:link properties'
   );
 });
